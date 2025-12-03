698a30d772632dc863cafdd59b9ff4377617bba8
who <24455370@qq.com>
2025-12-03 16:01:18 +0800

    feat: 打包成bundle，但路径还有问题

---
+++ src/bundler_2.ts
@@ -0,0 +1,112 @@
+/** 递归收集嵌套依赖 */
+import * as path from 'path';
+import * as fs from 'fs';
+import { parse } from '@babel/parser';
+import traverse from '@babel/traverse';
+import * as babel from '@babel/core';
+
+// 项目绝对路径
+const projectRootDir = path.resolve(__dirname, './bundler_2');
+// 声明依赖图的类型
+type DepRelation = Array<{ key: string; deps: string[]; code: string }>;
+// 初始化依赖图
+const depRelation: DepRelation = [];
+// 获取项目文件相对项目根目录的路径
+const getProjectPath = (dir: string) => {
+  return path.relative(projectRootDir, dir);
+};
+const wrapperFunction = (code: string) => {
+  return `function(require,module,exports){${code}}`;
+};
+const transformDepRelation = (_depRelation: DepRelation) => {
+  const items = _depRelation
+    .map((item) => {
+      return `
+      {
+        key: ${JSON.stringify(item.key)},
+        deps: ${JSON.stringify(item.deps)},
+        code: ${item.code}
+      }`;
+    })
+    .join(',');
+  return `[${items}]`;
+};
+// 收集依赖的函数。从入口开始收集
+const collect = (entryDir: string) => {
+  // 获取源代码
+  const code = fs.readFileSync(entryDir, { encoding: 'utf-8' });
+  // 源文件相对根目录的路径
+  const entryProjectPath = getProjectPath(entryDir);
+  // 分析过的模块不再分析
+  if (depRelation.find((i) => i.key === entryProjectPath)) {
+    console.warn('有重复依赖', entryProjectPath);
+    return;
+  }
+  // 转成es5代码
+  const es5Result = babel.transform(code, {
+    presets: ['@babel/preset-env'],
+  });
+  // 添加对应的记录
+  const depRecord: DepRelation[0] = {
+    key: entryProjectPath,
+    deps: [],
+    code: wrapperFunction(es5Result?.code || ''),
+  };
+  depRelation.push(depRecord);
+  // 转成ast
+  const ast = parse(code, { sourceType: 'module' });
+  // 利用ast收集依赖关系
+  traverse(ast, {
+    enter: (item) => {
+      if (item.node.type === 'ImportDeclaration') {
+        // 依赖的绝对路径
+        const depDir = path.resolve(
+          path.dirname(entryDir),
+          item.node.source.value
+        );
+        // 依赖相对于项目根目录的路径
+        const depProjectPath = getProjectPath(depDir);
+        depRecord.deps.push(depProjectPath);
+        // 递归收集
+        collect(depDir);
+      }
+    },
+  });
+};
+
+const getDistString = () => {
+  return `
+  const depRelation = ${transformDepRelation(depRelation)}
+  // 缓存模块的导出
+  const modulesCache = {};
+  const execute = (key) => {
+    if (!key) return;
+    if (modulesCache[key]) return modulesCache[key];
+    const item = depRelation.find((i) => i.key === key);
+    const require = (path) => {
+      return execute(path);
+    };
+    const exports = {
+        __esModule: true,
+      }
+    const module = {
+      exports: exports,
+    };
+    modulesCache[key]=module.exports;
+    // 执行模块
+    item.code(require, module, module.exports);
+    // 返回模块的导出
+    return module.exports;
+  };
+  execute(depRelation[0]?.key || '');`;
+};
+
+const bundler = () => {
+  collect(path.resolve(projectRootDir, './index.js'));
+  const distString = getDistString();
+  fs.rmdirSync(path.resolve(process.cwd(), 'dist'), { recursive: true });
+  fs.mkdirSync(path.resolve(process.cwd(), 'dist'), { recursive: true });
+  fs.writeFileSync(path.resolve(process.cwd(), 'dist/index.js'), distString);
+};
+
+bundler();
